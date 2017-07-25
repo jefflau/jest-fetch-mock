07e723c0eaca7ac1fc6aa931a8ec83075d07d96f
Alexey ERYSHEV <alexey.eryshev@canal-plus.com>
2017-07-25 15:51:21 +0200

    Add typings.

---
+++ src/index.d.ts
@@ -0,0 +1,16 @@
+declare interface MockParams {
+  status?: string;
+  statusText?: string;
+  url?: string;
+  headers?: Object;
+}
+
+declare namespace fetch {
+  function mockResponse(body: string, init?: MockParams): void;
+  function mockResponseOnce(body: string, init?: MockParams): void;
+  function mockResponses(responses: Array<{body: string, init?: MockParams}>): void;
+  function resetMocks(): void;
+  function mockReject(): void;
+  function mockRejectOnce(): void;
+  function resetMocks(): void;
+}
