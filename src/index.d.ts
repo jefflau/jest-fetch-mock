ec2f3ec8d17b3b81b59b898e170fcd047cf0f9cf
Jason McTaggart <JasonMcTaggart@me.com>
2018-06-12 19:15:13 -0400

    Updating typing to include one alias
    As per https://www.npmjs.com/package/jest-fetch-mock#mocking-multiple-fetches-with-different-responses

--- src/index.d.ts
+++ src/index.d.ts
@@ -12,6 +12,7 @@ declare module "jest-fetch-mock" {
     (input?: string | Request, init?: RequestInit): Promise<Response>;
     mockResponse(body: string, init?: MockParams): Fetch;
     mockResponseOnce(body: string, init?: MockParams): Fetch;
+    once(body: string, init?: MockParams): Fetch;
     mockResponses(...responses : Array<[string] | [string, MockParams]>): Fetch;
     mockReject(error?: Error): Fetch;
     mockRejectOnce(error?: Error): Fetch;
