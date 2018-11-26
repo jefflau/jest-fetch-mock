8c4a3a108385ab4e086f0212f093c65101c54cc7
lsimone <loris32@gmail.com>
2018-11-26 23:54:07 +0100

    added async support (#75)
    * added async support
    
    * promises are accepted as first argument instead of body
    * README and typescript interfaces updated
    
    * small refactor
    
    * functions are accepted as first argument instead of promises
    
    * typescript updated
    
    * readme example fixed
    
    * README + normalizeError fixed, tests added
    
    * removed IDE files, added folder to gitignore

--- src/index.js
+++ src/index.js
@@ -36,40 +36,34 @@ function ResponseWrapper(body, init) {
   return new ActualResponse(body, init)
 }
 
+const isFn = unknown => typeof unknown === 'function'
+
+const normalizeResponse = (bodyOrFunction, init) => () => isFn(bodyOrFunction) ?
+    bodyOrFunction().then(({body, init}) => new ResponseWrapper(body, init)) :
+    Promise.resolve(new ResponseWrapper(bodyOrFunction, init))
+
+const normalizeError = errorOrFunction =>  isFn(errorOrFunction) ?
+  errorOrFunction :
+  () => Promise.reject(errorOrFunction)
+
 const fetch = jest.fn()
 fetch.Headers = Headers
 fetch.Response = ResponseWrapper
 fetch.Request = Request
-fetch.mockResponse = (body, init) => {
-  return fetch.mockImplementation(() =>
-    Promise.resolve(new ResponseWrapper(body, init))
-  )
-}
+fetch.mockResponse = (bodyOrFunction, init) => fetch.mockImplementation(normalizeResponse(bodyOrFunction, init))
 
-fetch.mockReject = error => {
-  return fetch.mockImplementation(() => Promise.reject(error))
-}
+fetch.mockReject = errorOrFunction => fetch.mockImplementation(normalizeError(errorOrFunction))
 
-const mockResponseOnce = (body, init) => {
-  return fetch.mockImplementationOnce(() =>
-    Promise.resolve(new ResponseWrapper(body, init))
-  )
-}
+const mockResponseOnce = (bodyOrFunction, init) => fetch.mockImplementationOnce(normalizeResponse(bodyOrFunction, init))
 
 fetch.mockResponseOnce = mockResponseOnce
 
 fetch.once = mockResponseOnce
 
-fetch.mockRejectOnce = error => {
-  return fetch.mockImplementationOnce(() => Promise.reject(error))
-}
+fetch.mockRejectOnce = errorOrFunction => fetch.mockImplementationOnce(normalizeError(errorOrFunction))
 
 fetch.mockResponses = (...responses) => {
-  responses.forEach(([body, init]) => {
-    fetch.mockImplementationOnce(() =>
-      Promise.resolve(new ResponseWrapper(body, init))
-    )
-  })
+  responses.forEach(([bodyOrFunction, init]) => fetch.mockImplementationOnce(normalizeResponse(bodyOrFunction, init)))
   return fetch
 }
 
