7e7ce1bf7aade3570644733e67dadb61c7ba9746
Jonathan Fleckenstein <jonfleck88@gmail.com>
2020-07-21 23:21:27 -0400

    Update index.d.ts
    For users that need to mock the redirected boolean https://developer.mozilla.org/en-US/docs/Web/API/Response/redirected node fetch supports a counter option that when set to greater than 0 will respond to .redirected with true https://github.com/node-fetch/node-fetch/blob/master/src/response.js#L39.

--- types/index.d.ts
+++ types/index.d.ts
@@ -86,6 +86,7 @@ export interface MockParams {
     statusText?: string;
     headers?: string[][] | { [key: string]: string }; // HeadersInit
     url?: string;
+    counter?: number; // Set >= 1 to have redirected return true. https://github.com/node-fetch/node-fetch/blob/master/src/response.js#L39
 }
 
 export interface MockResponseInit extends MockParams {
